5e2bd0df872fef52c60f657c8f866a3ed53b4fc4
farheen2706 <afabdfais@gmail.com>
2020-11-15 19:01:51 +0530

    seperated main screen

---
+++ flashcards_apps/lib/models/QndA.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+class QandA {
+  final String id;
+  final String question;
+  final String answer;
+
+  QandA({
+    @required this.id,
+    @required this.question,
+    @required this.answer,
+  });
+}
